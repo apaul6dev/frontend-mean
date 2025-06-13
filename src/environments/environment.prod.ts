c087a105b34075f26ad177681ec4488e51b2f928
Paul Gallegos Aguilar <apaul.gallegos@gmail.com>
2025-06-13 07:04:02 -0500

    fix urkl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://internal-backend-alb-2101460653.us-east-1.elb.amazonaws.com:8081/api'
+  apiUrl: '/api'
 };
