b784dbdefc2f26e51cae536004107b53b154d98c
Paul Gallegos Aguilar <apaul.gallegos@gmail.com>
2025-06-13 06:20:02 -0500

    change dns production

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
-// src/environments/environment.ts
+//src/environments/environment.ts
 export const environment = {
     production: false,
     apiUrl: 'http://internal-backend-alb-2101460653.us-east-1.elb.amazonaws.com:8081/api'
